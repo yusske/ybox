bb63f2355e0909bd5bbe80c7cccf2964da171b23
yusske <jusseferrer@gmail.com>
2015-03-04 19:54:52 -0500

    Revert be63360..c7e2dbb
    This rolls back to commit be63360f56ace9d0e7fb9c3c7b55f3e759493f03.

--- dist/js/navigationConstants.js
+++
@@ -1,2 +0,0 @@
-/*! ybox 2015-03-04 */
-define(function(){return navigationConstants});
--- js/app/navigationConstants.js
+++
@@ -1,6 +0,0 @@
-define(function () {
-  var serviceURI = {
-    GET_PLAYLIST:'service/getplaylist.php'
-  };
-  return navigationConstants;
-});
