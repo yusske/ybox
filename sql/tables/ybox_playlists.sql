2e066793c1f55b0d9c2a1c30c90beda6b7b03589
Marco Hernandez <marcoh@MacBook-Pro-de-Marco.local>
2015-03-15 23:55:06 -0500

    removed db index temporarely
    commented the unique index and allowed track_id to be NULL, this is to
    allow testing while providing invalid track id’s

--- sql/tables/ybox_playlists.sql
+++ sql/tables/ybox_playlists.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS ybox_playlists;
 
 CREATE TABLE ybox_playlists (
 	id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	track_id VARCHAR(32) NOT NULL,
+	track_id VARCHAR(32) NULL,
 	artist VARCHAR(64) NOT NULL,
 	track VARCHAR(64) NOT NULL,
 	counter INT NOT NULL DEFAULT 0,
@@ -12,7 +12,7 @@ CREATE TABLE ybox_playlists (
 	user_id BIGINT NOT NULL,
 	status ENUM('new','played','viewed') NOT NULL DEFAULT 'new',
 	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT UNIQUE UN_ybox_playlists_source (mode, slug, track_id),
+	#CONSTRAINT UNIQUE UN_ybox_playlists_source (mode, slug, track_id),
 	INDEX IX_ybox_playlist_user_id (user_id)
 );
 
